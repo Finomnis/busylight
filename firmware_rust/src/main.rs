d5b252bacde5ef99ca588829a50a95c435f7b7f4
Martin Stumpf <martin.stumpf@vected.de>
2026-04-15 15:40:59 +0200

    Add multiple leds

--- firmware_rust/src/main.rs
+++ firmware_rust/src/main.rs
@@ -17,20 +17,23 @@ bind_interrupts!(struct Irqs {
     EXTI4_15 => exti::InterruptHandler<interrupt::typelevel::EXTI4_15>;
 });
 
+fn bit(val: u8, pos: u8) -> u8 {
+    return (val >> pos) & 1;
+}
+
+const NUM_LEDS: usize = 5;
+
 async fn set_led(mut spi: impl embedded_hal_async::spi::SpiBus<u8>, color: (u8, u8, u8)) {
     info!("Color: {:?}", color);
-    let mut data = [0u8; 14];
+    let mut data = [0u8; 12 * NUM_LEDS + 2];
     for (color_pos, &color_val) in [color.1, color.0, color.2].iter().enumerate() {
-        let color_encoded = &mut data[(color_pos * 4 + 1)..(color_pos * 4 + 5)];
-
-        fn bit(val: u8, pos: u8) -> u8 {
-            return (val >> pos) & 1;
+        for led_id in 0..NUM_LEDS {
+            let data_start = led_id * 12 + color_pos * 4 + 1;
+            data[data_start + 0] = 0x88 + 0x60 * bit(color_val, 7) + 0x06 * bit(color_val, 6);
+            data[data_start + 1] = 0x88 + 0x60 * bit(color_val, 5) + 0x06 * bit(color_val, 4);
+            data[data_start + 2] = 0x88 + 0x60 * bit(color_val, 3) + 0x06 * bit(color_val, 2);
+            data[data_start + 3] = 0x88 + 0x60 * bit(color_val, 1) + 0x06 * bit(color_val, 0);
         }
-
-        color_encoded[0] = 0x88 + 0x60 * bit(color_val, 7) + 0x06 * bit(color_val, 6);
-        color_encoded[1] = 0x88 + 0x60 * bit(color_val, 5) + 0x06 * bit(color_val, 4);
-        color_encoded[2] = 0x88 + 0x60 * bit(color_val, 3) + 0x06 * bit(color_val, 2);
-        color_encoded[3] = 0x88 + 0x60 * bit(color_val, 1) + 0x06 * bit(color_val, 0);
     }
     spi.write(&data).await.unwrap();
 }
